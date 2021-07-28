99017b6b965894846501d488d455d453741724c2
LuisEduardoLopez33 <193274@ids.upchiapas.edu.mx>
2021-07-28 01:13:54 -0500

    comit de nuevo

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,5 +1,6 @@
 import React,{useState, useEffect, useRef} from 'react'
 import css from '../assets/css/PostCreate.css'
+import Post from "./post";
 import socket from "../components/socket";
 import img1 from "../assets/imagenes/puestaSol.jpg"
 import img2 from "../assets/imagenes/Foto1.jpg"
@@ -42,6 +43,14 @@ const feed = ({nombre}) => {
       setImagen("");
 
     }
+
+function selectImage (id, e){
+    e.preventDefault();
+      alert(id);
+      setImagen(id);
+    }
+
+
         return(
             <div id="fondo">
                 <h1 id="titulo">Tuiter</h1>
@@ -52,16 +61,25 @@ const feed = ({nombre}) => {
                     <div className="alert " id="infoColor" role="alert" >
                         Selecciona la foto que te guste
                     </div>
-                    <div className="col-sm-4 col-md-2 col-lg-2" id="imagenes">
-                        <img src={img1} id="img1" width="150"height="150"/>
-                        <img src={img2} id="img2" width="150"height="150"/>
-                        <img src={img3} id="img3" width="150"height="150"/>
-                        <img src={img4} id="img4" width="150"height="150"/>
-                        <img src={img5} id="img5" width="150"height="150"/>
-                        <img src={img6} id="img6" width="150"height="150"/>
+                    <div className="col-sm-4 col-md-2 col-lg-2" id="imagenes" >
+                        <img src={img1} id="img1" width="150"height="150" onClick={(e) =>selectImage('img1', e)}/>
+                        <img src={img2} id="img2" width="150"height="150" onClick={(e) =>selectImage('img2', e)}/>
+                        <img src={img3} id="img3" width="150"height="150" onClick={(e) =>selectImage('img3', e)}/>
+                        <img src={img4} id="img4" width="150"height="150" onClick={(e) =>selectImage('img4', e)}/>
+                        <img src={img5} id="img5" width="150"height="150" onClick={(e) =>selectImage('img5', e)}/>
+                        <img src={img6} id="img6" width="150"height="150" onClick={(e) =>selectImage('img6', e)}/>
                     </div>
                     <button id="botonPublicar" onClick={submit}>Publicar</button>
 
+                </div>
+                <div className ="container">
+                    <div className="row">
+                    {publicaciones.map((e, i)=>(
+                        <div key={i}>
+                            <Post nombre ={e.nombre} publicacion={e.publicacion} imagen={e.imagen} ></Post>
+                        </div>
+                    ))}
+                    </div>
                 </div>
             </div>
         )
--- src/pages/post.js
+++ src/pages/post.js
@@ -1,33 +1,35 @@
 import React from 'react'
 import css from "../assets/css/post.css"
-import prueba from "../assets/imagenes/puestaSol.jpg"
+import img1 from "../assets/imagenes/puestaSol.jpg"
+import img2 from "../assets/imagenes/Foto1.jpg"
+import img3 from "../assets/imagenes/foto2.jpg"
+import img4 from "../assets/imagenes/foto3.jpg"
+import img5 from "../assets/imagenes/foto4.jpg"
+import img6 from "../assets/imagenes/foto5.jpg"
 import corazon from "../assets/SVG/favorite_border_white_24dp.svg"
-import corazonlleno from "../assets/SVG/favorite_border_white_24dp.svg"
 import comentario from "../assets/SVG/chat_bubble_white_24dp.svg"
 class post extends React.Component{
+    constructor(props) {
+        super(props);
+
+    }
+
     render() {
         return(
-            <div className="fondo">
-                <section id="bordepost">
-                    <div id="header">
-                        <h2 id="username">Luis Paulo</h2>
-                        <p id="descripcion">Los atardeceres son la obra mas bonita de Dios</p>
+            <React.Fragment>
+                <div className = "row">
+                    <div className="col" >
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">{this.props.nombre}</h5>
+                            <p className="card-text"></p>
+                            <p className="card-text"><small className="text-muted">{this.props.publicacion}</small></p>
+                        </div>
+                        <img src={this.props.imagen} className="card-img-bottom" />
                     </div>
-                    <div  id="card" >
-                        <img src={prueba} className="card-img-top" />
                     </div>
-                    <footer>
-                        <button type="button" id="corazonColor">
-                                <img id="borderColor" src={corazon} alt="" width="50" height="40"/>
-                        </button>
-                        <button type="button" id="comentario">
-                            <img id="borderColor" src={comentario} alt="" width="50" height="40"/>
-                        </button>
-                        <textarea id="inputComentario" name="comentarios"  placeholder="Comentario"/>
-                        <button id="botonComentario" >Comentar</button>
-                    </footer>
-                </section>
-            </div>
+                </div>
+            </React.Fragment>
         )
 
 
