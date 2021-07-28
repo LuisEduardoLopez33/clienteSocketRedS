54b9e2e066865429a8eb77ff97c116fbf9bf9ef4
luispaulo0 <193279@ids.upchiapas.edu.mx>
2021-07-27 19:25:16 -0500

    barra de imagenes

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,11 +1,20 @@
 import React,{useState, useEffect, useRef} from 'react'
 import css from '../assets/css/PostCreate.css'
 import socket from "../components/socket";
-
+import img1 from "../assets/imagenes/puestaSol.jpg"
+import img2 from "../assets/imagenes/Foto1.jpg"
+import img3 from "../assets/imagenes/foto2.jpg"
+import img4 from "../assets/imagenes/foto3.jpg"
+import img5 from "../assets/imagenes/foto4.jpg"
+import img6 from "../assets/imagenes/foto5.jpg"
+import Card from "../components/Card"
 
 
 
 const feed = ({nombre}) => {
+
+
+
     const [publicacion, setPublicacion] = useState("");
     const [publicaciones, setPublicaciones] = useState([]);
     const [imagen, setImagen] = useState("");
@@ -43,7 +52,16 @@ const feed = ({nombre}) => {
                     <div className="alert " id="infoColor" role="alert" >
                         Selecciona la foto que te guste
                     </div>
+                    <div className="col-sm-4 col-md-2 col-lg-2" id="imagenes">
+                        <img src={img1} id="img1" width="150"height="150"/>
+                        <img src={img2} id="img2" width="150"height="150"/>
+                        <img src={img3} id="img3" width="150"height="150"/>
+                        <img src={img4} id="img4" width="150"height="150"/>
+                        <img src={img5} id="img5" width="150"height="150"/>
+                        <img src={img6} id="img6" width="150"height="150"/>
+                    </div>
                     <button id="botonPublicar" onClick={submit}>Publicar</button>
+
                 </div>
             </div>
         )
